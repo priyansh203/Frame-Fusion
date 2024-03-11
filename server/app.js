b8e17fd7111ca3c92fea88d6911e61ddd3f9c415
Priyansh Dobariya <priyansh@Priyanshs-MacBook-Air.local>
2024-03-11 19:55:08 +0530

    devlopment changes14

--- server/app.js
+++ server/app.js
@@ -1,82 +1,57 @@
-const express = require('express')
+const express = require('express');
 require("dotenv").config();
 const cors = require('cors');
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const app = express()
-// const cookieParser = require("cookie-parser");
+const app = express();
 const path = require('path');
 const { putObject } = require('./s3');
 
 require("./config/configuration").connect();
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://frame-fusion.vercel.app");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  );
-  next();
-});
 
+if (!process.env.JWT_SECERT) {
+  console.log("FATAL ERROR: JWT_SECRET is not defined.");
+  process.exit(1);
+}
 
+// CORS middleware
+app.use(cors({ credentials: true, origin: "https://frame-fusion.vercel.app" }));
+
+// Body parser middleware
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(bodyParser.json({ limit: "50mb", extended: true }));
+
+// Routes
 const artistRoutes = require("./routes/artistRoutes");
 const userRoutes = require("./routes/userRoutes");
 const authRoutes = require("./routes/authRoutes");
 const artRoutes = require("./routes/artRoutes");
 const orderRoutes = require("./routes/orderRoutes");
 
-if(!process.env.JWT_SECERT){
-  console.log("FATAL ERROR: JWT_SECERT is not defined.");
-  process.exit(1);
-}
-
-
-//mongoose connection
-app.use(cors({credentials: true, origin: true})); 
-
-//use bodyparser
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
-app.use(bodyParser.json({ limit: "50mb", extended: true }));
-
-app.use(express.json())
-
-//routes
-app.use("/user",userRoutes)
+app.use("/user", userRoutes);
 app.use("/artist", artistRoutes);
 app.use("/api/auth", authRoutes);
-app.use("/art",artRoutes)
-app.use("/order",orderRoutes);
-
-
+app.use("/art", artRoutes);
+app.use("/order", orderRoutes);
 
 app.post('/api/upload', async (req, res) => {
   try {
-      let { filename, contentType } = req.body;
-      const currentDateTime = new Date().toISOString().replace(/[:.]/g, '-');
-      filename = currentDateTime+"-"+filename;
-      const url = await putObject(filename, contentType);
-      const objectUrl = `https://framefusion-art.s3.ap-south-1.amazonaws.com//uploads/user-uploads/${encodeURIComponent(filename)}`;
+    let { filename, contentType } = req.body;
+    const currentDateTime = new Date().toISOString().replace(/[:.]/g, '-');
+    filename = currentDateTime + "-" + filename;
+    const url = await putObject(filename, contentType);
+    const objectUrl = `https://framefusion-art.s3.ap-south-1.amazonaws.com//uploads/user-uploads/${encodeURIComponent(filename)}`;
 
-
-      res.json({ url, objectUrl });
+    res.json({ url, objectUrl });
   } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
-// app.use("/api/community", community); //it will display latest uploads and latest artist profile
-
-
-
-
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is starting at PORT ${PORT}`);
-  });
-
-
+  console.log(`Server is starting at PORT ${PORT}`);
+});
